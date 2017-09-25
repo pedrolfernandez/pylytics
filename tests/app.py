b2b668f3ca9ad1bdfabd4370e4bcfba99ed38c3f
Pedro Luis Fernandez Moreno <pedrol.fernandez@gmail.com>
2017-09-25 10:23:04 +0000

    app port is now 8080

--- tests/app.py
+++ tests/app.py
@@ -82,4 +82,4 @@ def upload_file():
     return 'file uploaded successfully'
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0' )
+    app.run(debug=True, host='0.0.0.0', port=8080)
